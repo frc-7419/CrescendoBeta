d04ece487166a5d65521e00c59c7d3a019952021
MostOriginalIGN <astrogamer543@gmail.com>
2024-11-07 15:33:30 -0800

    armsubsystem

---
+++ src/main/java/frc/robot/constants/DeviceIDs.java
@@ -0,0 +1,45 @@
+package frc.robot.constants;
+
+public final class DeviceIDs {
+    public enum CanIds {
+        // Arm CAN IDs
+        shooterWrist(13),
+
+        // Gyro CAN IDss
+        pigeon(16),
+
+        wrist(18),
+        leftIntakeMotor(7),
+        rightIntakeMotor(8),
+        climber(30),
+
+        topShooter(19),
+        bottomShooter(20),
+        serializerBack(21),
+        ;
+
+        public final int id;
+
+        CanIds(int id) {
+            this.id = id;
+        }
+    }
+
+    public enum SensorIds {
+
+        // Beambreak DIO ID
+        beamBreak(2),
+
+        // Limit switch DIO ID
+        limitSwitch(0),
+
+        // Arm Through Bore Encoder DIO ID
+        armEncoder(5);
+
+        public final int id;
+
+        SensorIds(int id) {
+            this.id = id;
+        }
+    }
+}
---
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -0,0 +1,15 @@
+package frc.robot.constants;
+
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Rotations;
+
+import edu.wpi.first.units.measure.Angle;
+
+public class RobotConstants {
+    public static final String canBus = "Ryan Biggee";
+
+    public static class ShooterConstants {
+        public static final Angle armOffset = Degrees.of(40);
+        public static final Angle encoderOffset = Rotations.of(0.587);
+    }
+}
